164c4a719c1b65b22b3616df8a5f8ba5c3677df9
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2025-04-16 17:01:35 +0200

    c9

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,7 +2,7 @@
 namespace App\Controller;
 
 use App\Model\Starship;
-
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,8 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route("/api/starships")]
-    public function getCollection() : Response 
+    public function getCollection(LoggerInterface $logger) : Response 
     {
+        //dd($logger);
+        $logger->info('collection de vaisseux spatiauxrécupérée');
         $starships = [
             new Starship(
                 id: 1,
