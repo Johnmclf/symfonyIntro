7f2a4f99159c0b6b7e4eb7cb364ffb5d81f13517
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2025-04-16 17:31:05 +0200

    Add files via upload

---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Repository;
+
+use App\Model\Starship;
+use Psr\Log\LoggerInterface;
+
+
+class StarshipRepository
+{
+
+    public function __construct(private LoggerInterface $logger)
+    {
+
+    }
+
+
+    public function findAll(): array
+    {
+        $this->logger->info('collection de vaisseux spatiauxrécupérée');
+        return [
+            new Starship(
+                id: 1,
+                name: 'USS LeafyCruiser (NCC-0001)',
+                class: 'Garden',
+                captain: 'Jean-Luc Pickles',
+                status: 'under construction',
+            ),
+            new Starship(
+                id: 1,
+                name: 'USS Espresso (NCC-1234-C)',
+                class: 'Latte',
+                captain: 'James T. Quick!',
+                status: 'repaired',
+            ),
+            new Starship(
+                id: 1,
+                name: 'USS Wanderlust (NCC-2024-W)',
+                class: 'Delta Tourist',
+                captain: 'Kathryn Journeyway',
+                status: 'under construction',
+            ),
+        ];
+    }
+}
